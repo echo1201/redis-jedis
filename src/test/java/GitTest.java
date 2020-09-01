129b2cdb0956ba8c685fc14da0fbc03b719212ee
echo <lingfangrao@gmail.com>
2020-09-01 19:43:48 +0800

    "first change"

---
+++ src/test/java/GitTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @Project redis-jedis
+ * @Author: Fang
+ * @Date: 2020/9/1 19:41
+ * @Description:
+ */
+public class GitTest {
+    @Test
+    public void test01() throws Exception{
+        System.out.println("can you hear me"); //edit by echo
+    }
+}
